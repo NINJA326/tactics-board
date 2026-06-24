50f58ba56d2c346f14275a99513c73e6d898d0f7
NINJA326 <xxxmastsxxx@gmail.com>
2026-06-25 02:02:04 +0900

    Add files via upload

---
+++ tactics-board-v13-3-pro-ui/sw.js
@@ -0,0 +1,5 @@
+const CACHE='tactics-board-v13-3-pro-ui';
+const FILES=['./','./index.html','./style.css','./app.js','./manifest.json','./assets/courts/full-court.png','./assets/courts/half-court.png'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
